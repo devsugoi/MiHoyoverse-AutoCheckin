9183558b53b2c38a2f11def29e5559ba66d7c88f
Matt Perez <matthew.perez.172000@gmail.com>
2025-05-10 01:54:59 +0800

    Start file debug remove

--- HoyoHelperStart.py
+++ HoyoHelperStart.py
@@ -8,5 +8,5 @@ import os
 
 # os.system('py Hoyoverse/Hoyo-os.py')
 # os.system('py Hoyoverse/Birthday-os.py')
-# os.system('py Hoyoverse/Hoyo-os.py&&py Hoyoverse/Birthday-os.py')
-os.system('py Hoyoverse/Birthday-os.py')
+os.system('py Hoyoverse/Hoyo-os.py&&py Hoyoverse/Birthday-os.py')
+#os.system('py Hoyoverse/Birthday-os.py')
