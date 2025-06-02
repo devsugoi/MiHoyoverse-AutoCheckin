b49bfcb2704668d095b8c82f42621f7f9c41ab7f
Matt Perez <matthew.perez.172000@gmail.com>
2025-06-03 01:45:23 +0800

    added custom announcement in bday script

--- Hoyoverse/Birthday-os.py
+++ Hoyoverse/Birthday-os.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     bday_list = []
     today = datetime.now()
     
+    # Check first if an environment variable belonging for the birthday script exists
     if os.getenv('BIRTHDAYS') != '':
         BIRTHDAYS = os.getenv('BIRTHDAYS')
     else:
@@ -22,12 +23,19 @@ if __name__ == '__main__':
     if os.getenv('DISCORD_WEBHOOK_SUMMARY') != '':
         allow_summary = True
     
+    # Divide the birthday list in the environment folder
     bday_list = BIRTHDAYS.split('#')
     log.info(f'Number of birthdays read: {len(bday_list)}')
     
+    # Loop each item in the list
     for bday in bday_list:
         bday_info = bday.split(';')
-        if today.strftime("%m/%d") == bday_info[1][-5:]:
+        
+        # Check if the item is not for ordinary birthday but for a custom announcement
+        if bday_info[0] == '!' and today.strftime("%Y/%m/%d") == bday_info[1]:
+            notify.send(app='Birthday Greeting Helper', status='', msg=bday_info[2],embed=False, isSummary=allow_summary)
+        
+        elif today.strftime("%m/%d") == bday_info[1][-5:]:
             greeting_list = [
                 f'Happy Birthday lods <@{bday_info[0]}>!',
                 f'Balita ko bday mo lode ah <@{bday_info[0]}>',
